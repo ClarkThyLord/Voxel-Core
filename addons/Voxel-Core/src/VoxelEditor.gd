d95f259afb41176f6d871dcc5186389c0f75e218
Christian Cuevas <ccpotential@gmail.com>
2019-12-14 10:16:59 -0800

    Update VoxelEditor.gd
    Clark was here :V

--- addons/Voxel-Core/src/VoxelEditor.gd
+++ addons/Voxel-Core/src/VoxelEditor.gd
@@ -140,6 +140,8 @@ func set_default_options(defaultoptions := {
 
 
 var VoxelObject : VoxelObjectClass setget edit
+var VoxelObjectData : Dictionary = {} setget set_voxel_object_data
+func set_voxel_object_data(voxelobjectdata : Dictionary) -> void: return   #   VoxelObjectData shouldn't be settable externally
 
 
 # Core
@@ -165,17 +167,40 @@ func edit(voxelobject : VoxelObjectClass, options := {}, emit := true) -> void:
 	
 	set_options(DefaultOptions if options.get('reset', false) else options)
 	VoxelObject = voxelobject
+	VoxelObjectData = {
+		'voxels': voxelobject.get_voxels(),               #   TODO: store only modified data, instead of all the data
+		'MeshType': voxelobject.MeshType,
+		'BuildStaticBody': voxelobject.BuildStaticBody,
+	}
+	voxelobject.set_mesh_type(VoxelObjectClass.MeshTypes.NAIVE, false, false)
+	voxelobject.set_build_static_body(true, false, false)
+	voxelobject.update()                                  #   TODO: update only modified meshes, instead of all meshes
 	
 	if emit: emit_signal('editing')
 
 signal committed
 func commit(emit := true) -> void:
-	cancel(false)
+	VoxelObject.set_mesh_type(VoxelObjectData['MeshType'], false, false)
+	VoxelObject.set_build_static_body(VoxelObjectData['BuildStaticBody'], false, false)
+	
+	VoxelObject.update()
+	
+	VoxelObject = null
+	VoxelObjectData = {}
+	undo_redo.clear_history()
+	
 	if emit: emit_signal('committed')
 
 signal canceled
 func cancel(emit := true) -> void:
+	VoxelObject.set_voxels(VoxelObjectData['voxels'], false)
+	VoxelObject.set_mesh_type(VoxelObjectData['MeshType'], false, false)
+	VoxelObject.set_build_static_body(VoxelObjectData['BuildStaticBody'], false, false)
+	
+	VoxelObject.update()
+	
 	VoxelObject = null
+	VoxelObjectData = {}
 	undo_redo.clear_history()
 	
 	if emit: emit_signal('canceled')
