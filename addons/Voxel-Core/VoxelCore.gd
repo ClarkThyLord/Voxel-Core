8a9502d3d5ac20896a17fff02e1a79fa899e0a24
Christian Cuevas <ccpotential@gmail.com>
2019-12-14 14:28:17 -0800

    functionating BottomPanel
    Clark was here :V

--- addons/Voxel-Core/VoxelCore.gd
+++ addons/Voxel-Core/VoxelCore.gd
@@ -2,7 +2,15 @@ tool
 extends EditorPlugin
 
 
+
+# Refrences
+const VoxelEditorEngineClass = preload('res://addons/Voxel-Core/engine/VoxelEditor.engine.gd')
+
+
+
 # Declarations
+var VoxelEdit := VoxelEditorEngineClass.new()
+
 var BottomPanel : ToolButton
 var BottomPanelControl := preload('res://addons/Voxel-Core/engine/gui/BottomPanel.tscn').instance()
 
@@ -12,10 +20,12 @@ var BottomPanelControl := preload('res://addons/Voxel-Core/engine/gui/BottomPane
 func _enter_tree():
 	add_autoload_singleton('VoxelSet', 'res://addons/Voxel-Core/defaults/VoxelSet.default.gd')
 	
-	BottomPanel = add_control_to_bottom_panel(BottomPanelControl, 'Voxel-Core')
-	make_bottom_panel_item_visible(BottomPanelControl)
-	
 	print('Loaded Voxel-Core.')
+
+func _ready():
+	BottomPanel = add_control_to_bottom_panel(BottomPanelControl, 'Voxel-Core')
+	BottomPanelControl.set_voxel_edit(VoxelEdit)
+	make_bottom_panel_item_visible(BottomPanelControl)
 
 func _exit_tree():
 	remove_autoload_singleton('VoxelSet')
--- addons/Voxel-Core/engine/gui/BottomPanel.gd
+++ addons/Voxel-Core/engine/gui/BottomPanel.gd
@@ -1,13 +1,58 @@
+tool
 extends Panel
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-# Called when the node enters the scene tree for the first time.
+
+# References
+const VoxelEditEngineClass := preload('res://addons/Voxel-Core/engine/VoxelEditor.engine.gd')
+
+onready var SettingsTabs := get_node('MarginContainer/HBoxContainer/Settings/TabContainer/')
+
+onready var FloorVisible := get_node('MarginContainer/HBoxContainer/Settings/TabContainer/Floor/HBoxContainer/CheckBox')
+onready var FloorColor := get_node('MarginContainer/HBoxContainer/Settings/TabContainer/Floor/HBoxContainer/ColorRect')
+
+onready var CursorVisible := get_node('MarginContainer/HBoxContainer/Settings/TabContainer/Cursor/HBoxContainer/CheckBox')
+onready var CursorColor := get_node('MarginContainer/HBoxContainer/Settings/TabContainer/Cursor/HBoxContainer/ColorRect')
+
+
+
+# Declarations
+signal set_voxel_edit(voxeledit)
+var VoxelEdit : VoxelEditEngineClass setget set_voxel_edit
+func set_voxel_edit(voxeledit : VoxelEditEngineClass, emit := true) -> void:
+	if not voxeledit == VoxelEdit:
+		if VoxelEdit:
+			VoxelEdit.disconnect('set_floor_visible', FloorVisible, 'set_toggle_mode')
+			FloorVisible.disconnect('toggled', VoxelEdit, 'set_floor_visible')
+			VoxelEdit.disconnect('set_floor_color', FloorColor, 'set_pick_color')
+			FloorColor.disconnect('color_changed', VoxelEdit, 'set_floor_color')
+			
+			VoxelEdit.disconnect('set_cursor_visible', CursorVisible, 'set_cursor_mode')
+			CursorVisible.disconnect('toggled', VoxelEdit, 'set_cursor_visible')
+			VoxelEdit.disconnect('set_cursor_color', CursorColor, 'set_cursor_color')
+			CursorColor.disconnect('color_changed', VoxelEdit, 'set_cursor_color')
+		
+		VoxelEdit = voxeledit
+		
+		VoxelEdit.connect('set_floor_visible', FloorVisible, 'set_toggle_mode')
+		FloorVisible.connect('toggled', VoxelEdit, 'set_floor_visible')
+		VoxelEdit.connect('set_floor_color', FloorColor, 'set_pick_color')
+		FloorColor.connect('color_changed', VoxelEdit, 'set_floor_color')
+		
+		VoxelEdit.connect('set_cursor_visible', CursorVisible, 'set_toggle_mode')
+		CursorVisible.connect('toggled', VoxelEdit, 'set_cursor_visible')
+		VoxelEdit.connect('set_cursor_color', CursorColor, 'set_pick_color')
+		CursorColor.connect('color_changed', VoxelEdit, 'set_cursor_color')
+		
+		if emit: emit_signal('set_voxel_edit', VoxelEdit)
+
+
+# Core
 func _ready():
-	pass # Replace with function body.
+	SettingsTabs.set_tab_disabled(0, true)
+	SettingsTabs.current_tab = 1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func set_floor_visible(visible : bool) -> void:
+	FloorVisible.pressed = visible
+
