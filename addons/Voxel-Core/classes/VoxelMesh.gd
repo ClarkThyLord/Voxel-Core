7925448e9f62853d39f851675d2d68dfc0706f04
Christian Cuevas <ccpotential@gmail.com>
2020-04-23 22:17:27 -0700

    added placeholder VoxelMesh
    Clark was here :V

---
+++ addons/Voxel-Core/classes/VoxelMesh.gd
@@ -0,0 +1,13 @@
+tool
+extends "res://addons/Voxel-Core/classes/VoxelObject.gd"
+class_name VoxelMesh, "res://addons/Voxel-Core/assets/classes/VoxelMesh.png"
+
+
+
+#
+# VoxelMesh, 
+#
+
+
+
+# Core
