816bc1af6f3bb3ca5315bc2ce4cfe6ec0813ba8c
Christian Cuevas <ccpotential@gmail.com>
2020-04-25 14:02:18 -0700

    progress on VoxelViewer
    Clark was here :V

--- addons/Voxel-Core/controls/VoxelViewer/VoxelViewer.gd
+++ addons/Voxel-Core/controls/VoxelViewer/VoxelViewer.gd
@@ -4,10 +4,18 @@ extends Control
 
 
 # Refrences
+onready var ViewerHint := get_node("ToolBar/Hint")
+
+
 onready var _2DView := get_node("2DView")
 
+
 onready var _3DView := get_node("3DView")
+
 onready var CameraPivot := get_node("3DView/Viewport/CameraPivot")
+
+onready var SelectPivot := get_node("3DView/Viewport/SelectPivot")
+onready var Select := get_node("3DView/Viewport/SelectPivot/Select")
 
 
 
@@ -25,12 +33,53 @@ func set_view_mode(view_mode : int) -> void:
 		_3DView.visible = ViewMode == ViewModes._3D
 
 
+export(Color) var SelectColor := Color("6400ffff") setget set_select_color
+func set_select_color(select_color : Color) -> void:
+	SelectColor = select_color
+	
+	if Select:
+		Select.material_override.albedo_color = SelectColor
+
+export(Vector3) var SelectedFace := Vector3.ZERO setget set_selected_face
+func set_selected_face(selected_face : Vector3) -> void:
+	SelectedFace = selected_face
+	
+	var hint := ""
+	var select_rot := Vector3.INF
+	match SelectedFace:
+		Vector3.RIGHT:
+			hint = "RIGHT"
+			select_rot = Vector3(0, 0, -90)
+		Vector3.LEFT:
+			hint = "LEFT"
+			select_rot = Vector3(0, 0, 90)
+		Vector3.UP:
+			hint = "UP"
+			select_rot = Vector3.ZERO
+		Vector3.DOWN:
+			hint = "DOWN"
+			select_rot = Vector3(180, 0, 0)
+		Vector3.FORWARD:
+			hint = "FRONT"
+			select_rot = Vector3(-90, 0, 0)
+		Vector3.BACK:
+			hint = "BACK"
+			select_rot = Vector3(90, 0, 0)
+	
+	if SelectPivot:
+		SelectPivot.visible = not select_rot == Vector3.INF
+		if not select_rot == Vector3.INF:
+			SelectPivot.rotation_degrees = select_rot
+	if ViewerHint:
+		ViewerHint.text = hint
+
+
 export(int, 0, 100) var MouseSensitivity := 6
 
 
 
 # Core
-func _on_3DView_gui_input(event : InputEvent):
+func _unhandled_input(event):
 	match ViewMode:
 		ViewModes._2D:
 			pass
@@ -39,13 +88,21 @@ func _on_3DView_gui_input(event : InputEvent):
 				if event.button_index == BUTTON_LEFT:
 					if event.is_pressed():
 						dragging = true
-						_3DView.mouse_default_cursor_shape = Control.CURSOR_MOVE
+						Input.set_default_cursor_shape(Control.CURSOR_MOVE)
 					else:
 						dragging = false
-						_3DView.mouse_default_cursor_shape = Control.CURSOR_ARROW
-					accept_event()
+						Input.set_default_cursor_shape(Control.CURSOR_ARROW)
 			elif dragging and event is InputEventMouseMotion:
 				var motion = event.relative.normalized()
 				CameraPivot.rotation_degrees.x += -motion.y * MouseSensitivity
 				CameraPivot.rotation_degrees.y += -motion.x * MouseSensitivity
-				accept_event()
+
+
+func _on_VoxelStaticBody_mouse_exited():
+	Input.set_default_cursor_shape(Control.CURSOR_ARROW)
+
+func _on_VoxelStaticBody_input_event(camera, event, click_position, click_normal, shape_idx):
+	if not dragging:
+		Input.set_default_cursor_shape(Control.CURSOR_POINTING_HAND)
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed() and event.doubleclick:
+		set_selected_face(click_normal.round())
