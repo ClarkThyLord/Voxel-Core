941a2e3e6c90af94e2f019ac9ce13fc19a56aaab
Christian Cuevas <ccpotential@gmail.com>
2020-04-24 20:29:34 -0700

    refined VoxelTool
    Clark was here :V

--- addons/Voxel-Core/classes/VoxelTool.gd
+++ addons/Voxel-Core/classes/VoxelTool.gd
@@ -11,6 +11,9 @@ class_name VoxelTool, "res://addons/Voxel-Core/assets/classes/VoxelTool.png"
 
 
 # Declarations
+var _index := 0 setget set_index
+func set_index(_index : int) -> void: pass
+
 export(bool) var VoxelUV := false setget set_voxel_uv
 func set_voxel_uv(voxel_uv : bool) -> void: pass
 
@@ -21,6 +24,7 @@ func set_voxel_size(voxel_size : float) -> void: pass
 
 # Core
 func start(voxel_uv := false, voxel_size := Voxel.VoxelSize) -> void:
+	_index = 0
 	VoxelUV = voxel_uv
 	VoxelSize = voxel_size
 	begin(Mesh.PRIMITIVE_TRIANGLES)
@@ -32,98 +36,55 @@ func end() -> Array:
 func add_face(
 	voxel : Dictionary,
 	normal : Vector3,
-	top_left : Vector3,
-	top_right := Vector3.INF,
-	bottom_left := Vector3.INF,
-	bottom_right := Vector3.INF
+	bottom_right_ : Vector3,
+	bottom_left_ := Vector3.INF,
+	top_right_ := Vector3.INF,
+	top_left_ := Vector3.INF
 	) -> void:
-	top_left = Voxel.grid_to_snapped(top_left)
-	if top_right == Vector3.INF: top_right = top_left
-	else: top_right = Voxel.grid_to_snapped(top_right)
-	if bottom_left == Vector3.INF: bottom_left = top_left
-	else: bottom_left = Voxel.grid_to_snapped(bottom_left)
-	if bottom_right == Vector3.INF: bottom_right = top_left
-	else: bottom_right = Voxel.grid_to_snapped(bottom_right)
+	bottom_right_ = bottom_right_
+	if bottom_left_ == Vector3.INF: bottom_left_ = bottom_right_
+	if top_right_ == Vector3.INF: top_right_ = bottom_right_
+	if top_left_ == Vector3.INF: top_left_ = bottom_right_
 	
 	add_normal(normal)
 	add_color(Voxel.get_color_side(voxel, normal))
 	
 	match normal:
 		Vector3.RIGHT:
-			add_vertex(top_left + Vector3.RIGHT)
-			
-			add_vertex(top_right + Vector3.RIGHT + Vector3.BACK)
-			
-			add_vertex(bottom_left + Vector3.RIGHT + Vector3.UP)
-			
-			
-			add_vertex(top_right + Vector3.RIGHT + Vector3.BACK)
-			
-			add_vertex(bottom_right + Vector3.ONE)
-			
-			add_vertex(bottom_left + Vector3.RIGHT + Vector3.UP)
+			add_vertex((bottom_right_ + Vector3.RIGHT) * VoxelSize)
+			add_vertex((bottom_left_ + Vector3.RIGHT + Vector3.BACK) * VoxelSize)
+			add_vertex((top_right_ + Vector3.RIGHT + Vector3.UP) * VoxelSize)
+			add_vertex((top_left_ + Vector3.ONE) * VoxelSize)
 		Vector3.LEFT:
-			add_vertex(bottom_right + Vector3.UP + Vector3.BACK)
-			
-			add_vertex(top_right + Vector3.BACK)
-			
-			add_vertex(bottom_left + Vector3.UP)
-			
-			
-			add_vertex(top_right + Vector3.BACK)
-			
-			add_vertex(top_left)
-			
-			add_vertex(bottom_left + Vector3.UP)
+			add_vertex((bottom_right_ + Vector3.BACK) * VoxelSize)
+			add_vertex((bottom_left_) * VoxelSize)
+			add_vertex((top_right_ + Vector3.UP + Vector3.BACK) * VoxelSize)
+			add_vertex((top_left_ + Vector3.UP) * VoxelSize)
 		Vector3.UP:
-			add_vertex(bottom_right + Vector3.ONE)
-			
-			add_vertex(top_right + Vector3.UP + Vector3.BACK)
-			
-			add_vertex(bottom_left + Vector3.RIGHT + Vector3.UP)
-			
-			
-			add_vertex(top_right + Vector3.UP + Vector3.BACK)
-			
-			add_vertex(top_left + Vector3.UP)
-			
-			add_vertex(bottom_left + Vector3.RIGHT + Vector3.UP)
+			add_vertex((bottom_right_ + Vector3.ONE) * VoxelSize)
+			add_vertex((bottom_left_ + Vector3.UP + Vector3.BACK) * VoxelSize)
+			add_vertex((top_right_ + Vector3.RIGHT + Vector3.UP) * VoxelSize)
+			add_vertex((top_left_ + Vector3.UP) * VoxelSize)
 		Vector3.DOWN:
-			add_vertex(top_left)
-			
-			add_vertex(top_right + Vector3.BACK)
-			
-			add_vertex(bottom_left + Vector3.RIGHT)
-			
-			
-			add_vertex(top_right + Vector3.BACK)
-			
-			add_vertex(bottom_right + Vector3.RIGHT + Vector3.BACK)
-			
-			add_vertex(bottom_left + Vector3.RIGHT)
+			add_vertex((bottom_right_ + Vector3.BACK) * VoxelSize)
+			add_vertex((bottom_left_  + Vector3.RIGHT + Vector3.BACK) * VoxelSize)
+			add_vertex((top_right_) * VoxelSize)
+			add_vertex((top_left_ + Vector3.RIGHT) * VoxelSize)
 		Vector3.FORWARD:
-			add_vertex(top_left)
-			
-			add_vertex(top_right + Vector3.RIGHT)
-			
-			add_vertex(bottom_left + Vector3.UP)
-			
-			
-			add_vertex(top_right + Vector3.RIGHT)
-			
-			add_vertex(bottom_right + Vector3.RIGHT + Vector3.UP)
-			
-			add_vertex(bottom_left + Vector3.UP)
+			add_vertex((bottom_right_) * VoxelSize)
+			add_vertex((bottom_left_ + Vector3.RIGHT) * VoxelSize)
+			add_vertex((top_right_ + Vector3.UP) * VoxelSize)
+			add_vertex((top_left_ + Vector3.RIGHT + Vector3.UP) * VoxelSize)
 		Vector3.BACK:
-			add_vertex(bottom_right + Vector3.ONE)
-			
-			add_vertex(top_right + Vector3.RIGHT + Vector3.BACK)
-			
-			add_vertex(bottom_left + Vector3.UP + Vector3.BACK)
-			
-			
-			add_vertex(top_right + Vector3.RIGHT + Vector3.BACK)
-			
-			add_vertex(top_left + Vector3.BACK)
-			
-			add_vertex(bottom_left + Vector3.UP + Vector3.BACK)
+			add_vertex((bottom_right_ + Vector3.RIGHT + Vector3.BACK) * VoxelSize)
+			add_vertex((bottom_left_ + Vector3.BACK) * VoxelSize)
+			add_vertex((top_right_ + Vector3.ONE) * VoxelSize)
+			add_vertex((top_left_ + Vector3.UP + Vector3.BACK) * VoxelSize)
+	
+	_index += 4
+	add_index(_index - 4)
+	add_index(_index - 3)
+	add_index(_index - 2)
+	add_index(_index - 3)
+	add_index(_index - 1)
+	add_index(_index - 2)
