f6a8cdaed1c8304ecea318352697bab434dc4331
Christian Cuevas <ccpotential@gmail.com>
2020-04-29 17:24:33 -0700

    working on VoxelSetEditor
    Clark was here :V

--- addons/Voxel-Core/Voxel-Core.gd
+++ addons/Voxel-Core/Voxel-Core.gd
@@ -3,9 +3,64 @@ extends EditorPlugin
 
 
 
+# Refrences
+const VoxelObject := preload("res://addons/Voxel-Core/classes/VoxelObject.gd")
+
+const VoxelSetEditor := preload("res://addons/Voxel-Core/engine/Editors/VoxelSet/VoxelSet.tscn")
+
+
+
+# Declarations
+var handling : Object
+
+var VoxelSetEditorRef
+
+
+
+# Helpers
+enum VoxelCore {
+	OTHER = -1,
+	VOXEL_SET,
+	VOXEL_OBJECT,
+	VOXEL_MESH
+}
+static func typeof_voxel_core(object : Object) -> int:
+	var type_of = VoxelCore.OTHER
+	if object is VoxelSet: type_of = VoxelCore.VOXEL_SET
+	elif object is VoxelObject: type_of = VoxelCore.VOXEL_OBJECT
+	elif object is VoxelMesh: type_of = VoxelCore.VOXEL_MESH
+	return type_of
+
+
+
 # Core
 func _enter_tree():
 	print("Voxel-Core is active...")
 
 func _exit_tree():
 	print("Voxel-Core is inactive!")
+
+
+func show_voxel_set_editor(voxelset : VoxelSet) -> void:
+	if not VoxelSetEditorRef:
+		VoxelSetEditorRef = VoxelSetEditor.instance()
+	add_control_to_bottom_panel(VoxelSetEditorRef, "VoxelSet")
+	make_bottom_panel_item_visible(VoxelSetEditorRef)
+
+func close_voxel_set_editor() -> void:
+	if VoxelSetEditorRef:
+		remove_control_from_bottom_panel(VoxelSetEditorRef)
+		hide_bottom_panel()
+
+
+func handles(object : Object) -> bool:
+	if typeof_voxel_core(object) == VoxelCore.VOXEL_SET:
+		handling = object
+		show_voxel_set_editor(handling)
+		return true
+	elif handling:
+		match typeof_voxel_core(handling):
+			VoxelCore.VOXEL_SET:
+				close_voxel_set_editor()
+		handling = null
+	return false
