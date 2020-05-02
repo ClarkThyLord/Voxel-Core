ef6080ce4168d63e3d58a809a016a266f6a82b31
Christian Cuevas <ccpotential@gmail.com>
2020-05-01 19:24:28 -0700

    saving and loading of meta data in VoxelSet
    Clark was here :V

--- addons/Voxel-Core/classes/VoxelSet.gd
+++ addons/Voxel-Core/classes/VoxelSet.gd
@@ -20,15 +20,23 @@ func get_id() -> int:
 	ids.sort()
 	return (ids.back() + 1) if ids.size() > 0 else 0
 
-var Names:= {} setget set_names, get_names
-func set_names(names : Dictionary) -> void: pass
-func get_names() -> Dictionary: return Names.duplicate()
-func name_to_id(name : String) -> int: return Names.get(name, -1)
-
-var Voxels := {} setget set_voxels, get_voxels
+var Names := {}
+func name_to_id(name : String) -> int:
+	return Names.get(name, -1)
 
 
-export(bool) var Expandable := true
+var Voxels := {} setget set_voxels
+func set_voxels(voxels : Dictionary, emit := true) -> void:
+	if Locked:
+		printerr("VoxelSet Locked")
+		return
+	
+	Voxels = voxels
+	
+	if emit: updated_voxels()
+
+
+export(bool) var Locked := false
 
 
 var UVScale := 0.0 setget set_uv_scale
@@ -38,18 +46,36 @@ export(float, 1, 1000000000, 1) var TileSize := 32.0 setget set_tile_size
 func set_tile_size(tile_size : float, update := true) -> void:
 	TileSize = floor(clamp(tile_size, 1, 1000000000))
 	
-	if update: update_uv()
+	if update: updated_textures()
 
 export(Texture) var Tiles : Texture = null setget set_tiles
 func set_tiles(tiles : Texture, update := true) -> void:
 	Tiles = tiles
 	
-	if update: update_uv()
+	if update: updated_textures()
 
 
 
 # Core
-func set_voxel(voxel : Dictionary, id = get_id(), emit := true) -> int:
+func _save() -> void:
+	set_meta("Names", Names)
+	set_meta("Voxels", Voxels)
+
+func _load() -> void:
+	if has_meta("Names"):
+		Names = get_meta("Names")
+	if has_meta("Voxels"):
+		Voxels = get_meta("Voxels")
+
+
+func _init(): call_deferred("_load")
+
+
+func set_voxel(voxel : Dictionary, id = get_id(), update := true) -> int:
+	if Locked:
+		printerr("VoxelSet Locked")
+		return -1
+	
 	if typeof(id) == TYPE_STRING:
 		if not Names.has(id):
 			Names[id] = get_id()
@@ -57,48 +83,57 @@ func set_voxel(voxel : Dictionary, id = get_id(), emit := true) -> int:
 	elif not typeof(id) == TYPE_INT:
 		printerr("invalid id given when setting voxel, id: ", str(id))
 		return -1
+	id = int(abs(id))
 	
 	Voxels[id] = voxel
 	
-	if emit: emit_signal("updated_voxels")
+	if update: updated_voxels()
 	
 	return id
 
-func set_voxels(voxels : Dictionary, emit := true) -> void:
-	erase_voxels()
-	for id in voxels:
-		if typeof(id) == TYPE_STRING and typeof(voxels[id]) == TYPE_DICTIONARY:
-			set_voxel(voxels[id], id)
-	
-	if emit: emit_signal("updated_voxels")
-
 func get_voxel(id) -> Dictionary:
-	return Voxels.get(name_to_id(id) if typeof(id) == TYPE_STRING else id, {}).duplicate(true)
+	if Locked:
+		printerr("VoxelSet Locked")
+		return {}
+	
+	return Voxels.get(name_to_id(id) if typeof(id) == TYPE_STRING else id, {})
 
-func get_voxels():
-	return Voxels.keys()
-
-func erase_voxel(id, emit := true) -> void:
+func erase_voxel(id, update := true) -> void:
+	if Locked:
+		printerr("VoxelSet Locked")
+		return
+	
 	if typeof(id) == TYPE_STRING:
 		var name = id
 		id = name_to_id(id)
-		Names.erase(id)
+		if id == -1:
+			printerr("invalid name given when erasing voxel, name: ", name)
+		Names.erase(name)
 	elif not typeof(id) == TYPE_INT:
-		printerr("invalid id given when erasing voxel, id: ", str(id))
+		printerr("invalid id given when erasing voxel, id: ", id)
+	id = int(abs(id))
 	
 	Voxels.erase(id)
 	
-	if emit: emit_signal("updated_voxels")
+	if update: updated_voxels()
 
-func erase_voxels(update := true, emit := true) -> void:
+func erase_voxels(update := true, update := true) -> void:
+	if Locked:
+		printerr("VoxelSet Locked")
+		return
+	
 	Names.clear()
 	Voxels.clear()
 	
-	if emit: emit_signal("updated_voxels")
+	if update: updated_voxels()
 
 
-func update_uv() -> void:
-	if TileSize > 0 and Tiles:
+func updated_voxels() -> void:
+	_save()
+	emit_signal("updated_voxels")
+
+func updated_textures() -> void:
+	if is_instance_valid(Tiles):
 		UVScale = 1.0 / (Tiles.get_width() / TileSize)
 	else: UVScale = 0.0
 	
