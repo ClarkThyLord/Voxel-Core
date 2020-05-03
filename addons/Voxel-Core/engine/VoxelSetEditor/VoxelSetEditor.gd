f432b7d8b4feecfb6b97f2ba7ec644a52d16fde3
Christian Cuevas <ccpotential@gmail.com>
2020-05-03 12:28:25 -0700

    fix up VoxelSetEditor
    Clark was here :V

--- addons/Voxel-Core/engine/VoxelSetEditor/VoxelSetEditor.gd
+++ addons/Voxel-Core/engine/VoxelSetEditor/VoxelSetEditor.gd
@@ -5,64 +5,77 @@ extends ScrollContainer
 
 # Refrences
 onready var VoxelSetInfo := get_node("HBoxContainer/VBoxContainer/VoxelSetInfo")
+
 onready var VoxelInfo := get_node("HBoxContainer/VBoxContainer/VoxelInfo")
+onready var VoxelID := get_node("HBoxContainer/VBoxContainer/VoxelInfo/HBoxContainer/VoxelID")
+onready var VoxelName := get_node("HBoxContainer/VBoxContainer/VoxelInfo/HBoxContainer/VoxelName")
+onready var VoxelData := get_node("HBoxContainer/VBoxContainer/VoxelInfo/VoxelData")
+
 
 onready var Duplicate := get_node("HBoxContainer/VBoxContainer2/ToolBar/Duplicate")
 onready var Remove := get_node("HBoxContainer/VBoxContainer2/ToolBar/Remove")
 
 onready var VoxelSetViewer := get_node("HBoxContainer/VBoxContainer2/VoxelSetViewer")
 
+
 onready var VoxelInspector := get_node("HBoxContainer/VoxelInspector")
-onready var VoxelView := get_node("HBoxContainer/VoxelInspector/VoxelViewer")
+onready var VoxelViewer := get_node("HBoxContainer/VoxelInspector/VoxelViewer")
 
 
 
 # Declarations
-var SelectedVoxel = -1 setget set_selected_voxel
-func set_selected_voxel(selected_voxel) -> void:
-	SelectedVoxel = selected_voxel
-	
-	if VoxelInfo:
-		VoxelInfo.visible = SelectedVoxel > -1
-		VoxelInfo.text = "ID: " + str(SelectedVoxel)
-		if Voxel_Set:
-			VoxelInfo.text += "\nRaw Data:\n"+ var2str(Voxel_Set.get_voxel(SelectedVoxel))
-	if Duplicate: Duplicate.visible = SelectedVoxel > -1
-	if Remove: Remove.visible = SelectedVoxel > -1
-	if VoxelInspector: VoxelInspector.visible = SelectedVoxel > -1
-	if VoxelView and SelectedVoxel > -1 and is_instance_valid(Voxel_Set):
-		VoxelView.setup_voxel(SelectedVoxel, Voxel_Set)
-
-var Voxel_Set : VoxelSet
+export(Resource) var Voxel_Set = load("res://addons/Voxel-Core/defaults/VoxelSet.tres") setget set_editing_voxel_set
+func set_editing_voxel_set(editing_voxel_set : Resource, update := true) -> void:
+	if editing_voxel_set is VoxelSet:
+		if Voxel_Set.is_connected("updated_voxels", self, "_update"):
+			Voxel_Set.disconnect("updated_voxels", self, "_update")
+		Voxel_Set = editing_voxel_set
+		Voxel_Set.connect("updated_voxels", self, "_update")
+		
+		if VoxelSetViewer:
+			VoxelSetViewer.Voxel_Set = Voxel_Set
+		
+		if update: _update()
+	elif typeof(editing_voxel_set) == TYPE_NIL:
+		set_editing_voxel_set(load("res://addons/Voxel-Core/defaults/VoxelSet.tres"), update)
 
 
 
 # Core
-func edit_voxel_set(voxelset : VoxelSet) -> void:
-	Voxel_Set = voxelset
-	set_selected_voxel(-1)
-	
+func _ready(): _update()
+
+
+func _update() -> void:
 	if VoxelSetInfo:
-		VoxelSetInfo.text = "Voxels: " + str(voxelset.Voxels.size())
-		VoxelSetInfo.text += "\nTiled: " + str(is_instance_valid(voxelset.Tiles))
-		VoxelSetInfo.text += "\nTile Size: " + str(Vector2.ONE * voxelset.TileSize)
+		VoxelSetInfo.text = "Voxels: " + str(Voxel_Set.Voxels.size())
+		VoxelSetInfo.text += "\nTiled: " + str(is_instance_valid(Voxel_Set.Tiles))
+		VoxelSetInfo.text += "\nTile Size: " + str(Vector2.ONE * Voxel_Set.TileSize)
+	
 	if VoxelSetViewer:
-		VoxelSetViewer.Voxel_Set = voxelset
+		if VoxelSetViewer.Selections.size() == 1:
+			var id = str(VoxelSetViewer.Selections[0])
+			if Duplicate: Duplicate.visible = true
+			if Remove: Remove.visible = true
+			
+			if typeof(id) == TYPE_STRING:
+				VoxelName.text = id
+				id = Voxel_Set.name_to_id(id)
+			VoxelID.text = str(id)
+			VoxelData.text = var2str(Voxel_Set.get_voxel(id))
+			
+			if VoxelInspector:
+				VoxelInspector.visible = true
+				VoxelViewer.setup_voxel(id, Voxel_Set)
+		else:
+			if Duplicate: Duplicate.visible = false
+			if Remove: Remove.visible = false
+			
+			if VoxelInspector:
+				VoxelInspector.visible = false
 
 
 func _on_Save_pressed():
 	ResourceSaver.save(Voxel_Set.resource_path, Voxel_Set.duplicate())
-
-
-func _on_VoxelSetViewer_selected(index):
-	if VoxelSetViewer.Selections.size() == 1:
-		set_selected_voxel(VoxelSetViewer.Selections[0])
-	else: set_selected_voxel(-1)
-
-func _on_VoxelSetViewer_unselected(index):
-	if VoxelSetViewer.Selections.size() == 1:
-		set_selected_voxel(VoxelSetViewer.Selections[0])
-	else: set_selected_voxel(-1)
 
 
 func _on_Add_pressed():
@@ -71,10 +84,11 @@ func _on_Add_pressed():
 
 func _on_Duplicate_pressed():
 	if is_instance_valid(Voxel_Set):
-		Voxel_Set.set_voxel(Voxel_Set.get_voxel(SelectedVoxel).duplicate(true))
-		set_selected_voxel(-1)
+		Voxel_Set.set_voxel(Voxel_Set.get_voxel(VoxelSetViewer.Selections[0]).duplicate(true))
 
 func _on_Remove_pressed():
 	if is_instance_valid(Voxel_Set):
-		Voxel_Set.erase_voxel(SelectedVoxel)
-		set_selected_voxel(-1)
+		Voxel_Set.erase_voxel(VoxelSetViewer.Selections[0])
+
+func _on_VoxelSetViewer_selected(index): _update()
+func _on_VoxelSetViewer_unselected(index): _update()
