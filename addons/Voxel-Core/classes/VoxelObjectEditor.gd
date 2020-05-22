3ac9e006265e3dfd10acf07c86409a63409bb5f0
Christian Cuevas <ccpotential@gmail.com>
2020-05-21 18:00:15 -0700

    VoxelObjectEditor
    Clark was here :V

--- addons/Voxel-Core/classes/VoxelObjectEditor.gd
+++ addons/Voxel-Core/classes/VoxelObjectEditor.gd
@@ -3,16 +3,53 @@ extends Reference
 class_name VoxelObjectEditor, "res://addons/Voxel-Core/assets/classes/VoxelEditor.png"
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+#
+# VoxelObjectEditor, 
+#
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+# Refrences
+const VoxelObject := preload("res://addons/Voxel-Core/classes/VoxelObject.gd")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+# Declarations
+signal set_tools
+
+var Tools := [] setget set_tools
+func set_tools(tools : Array) -> void:
+	Tools = tools
+	
+	emit_signal("set_tools")
+
+
+
+# Utilities
+func raycast(camera : Camera, screen_point : Vector2, ray_length := 1000.0, exclude := []) -> Dictionary:
+	var from = camera.project_ray_origin(screen_point)
+	var to = from + camera.project_ray_normal(screen_point) * ray_length
+	return camera.get_world().direct_space_state.intersect_ray(from, to, exclude)
+
+func raycast_for(targets : Array, camera : Camera, screen_point : Vector2, ray_length := 1000.0) -> Dictionary:
+	var exclude := []
+	var hit : Dictionary
+	var from = camera.project_ray_origin(screen_point)
+	var to = from + camera.project_ray_normal(screen_point) * 1000
+	while true:
+		hit = camera.get_world().direct_space_state.intersect_ray(from, to, exclude)
+		if not hit.empty():
+			for target in targets:
+				if target.is_a_parent_of(hit.collider):
+					hit["target"] = target
+					break
+			exclude.append(hit.collider)
+		else: break
+	return hit
+
+
+
+# Core
+func input(event : InputEvent, camera : Camera) -> void:
+	pass
