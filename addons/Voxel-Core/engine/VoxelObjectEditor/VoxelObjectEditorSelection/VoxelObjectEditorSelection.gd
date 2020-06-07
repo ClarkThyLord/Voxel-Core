6f767c7e44cc80f526b6a0c6c6bbd6f0c690e339
Christian Cuevas <ccpotential@gmail.com>
2020-06-07 13:47:44 -0700

    started modulating VoxelObjectEditorSelection
    Clark was here :V

---
+++ addons/Voxel-Core/engine/VoxelObjectEditor/VoxelObjectEditorSelection/VoxelObjectEditorSelection.gd
@@ -0,0 +1,13 @@
+tool
+extends Reference
+
+
+
+# Declarations
+export(String) var name := ""
+
+
+
+# Core
+func select(prev_hit : Dictionary, last_hit : Dictionary, event : InputEvent) -> void:
+	pass
---
+++ addons/Voxel-Core/engine/VoxelObjectEditor/VoxelObjectEditorSelection/VoxelObjectEditorSelections/Extrude.gd
@@ -0,0 +1,12 @@
+tool
+extends "res://addons/Voxel-Core/engine/VoxelObjectEditor/VoxelObjectEditorSelection/VoxelObjectEditorSelection.gd"
+
+
+
+# Core
+func _init():
+	name = "extrude"
+
+
+func select(prev_hit : Dictionary, last_hit : Dictionary, event : InputEvent) -> void:
+	pass
