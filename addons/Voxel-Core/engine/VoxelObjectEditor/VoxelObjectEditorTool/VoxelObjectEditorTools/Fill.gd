45e3735c6d2668274174697075539a0eb7e60d10
Christian Cuevas <ccpotential@gmail.com>
2020-06-09 17:56:28 -0700

    fill tool
    Clark was here :V

--- addons/Voxel-Core/engine/VoxelObjectEditor/VoxelObjectEditorTool/VoxelObjectEditorTools/Fill.gd
+++ addons/Voxel-Core/engine/VoxelObjectEditor/VoxelObjectEditorTool/VoxelObjectEditorTools/Fill.gd
@@ -11,5 +11,37 @@ func _init():
 	])
 
 
+func fill(voxel_object, position : Vector3, target, replacement, undo_redo : UndoRedo, filled := []) -> void:
+	var voxel = voxel_object.get_rvoxel(position)
+	var compare = voxel
+	if not typeof(voxel) == TYPE_NIL:
+		match typeof(voxel):
+			TYPE_DICTIONARY:
+				compare = Voxel.get_color(voxel)
+		if typeof(compare) == typeof(target) and compare == target and not filled.has(position):
+			filled.append(position)
+			undo_redo.add_do_method(voxel_object, 'set_voxel', position, replacement)
+			undo_redo.add_undo_method(voxel_object, 'set_voxel', position, voxel)
+			for direction in Voxel.Directions:
+				fill(voxel_object, position + direction, target, replacement, undo_redo, filled)
+
+
 func work(editor) -> void:
-	pass
+	editor.Undo_Redo.create_action("VoxelObjectEditor : Fill voxel(s)")
+	for selection in editor.get_selections():
+		for position in selection:
+			var target = editor.VoxelObjectRef.get_rvoxel(position)
+			if not typeof(target) == TYPE_NIL:
+				match typeof(target):
+					TYPE_DICTIONARY:
+						target = Voxel.get_color(target)
+				fill(
+					editor.VoxelObjectRef,
+					position,
+					target,
+					editor.get_rpalette() if editor.Raw.pressed else editor.get_palette(),
+					editor.Undo_Redo
+				)
+	editor.Undo_Redo.add_do_method(editor.VoxelObjectRef, "update_mesh")
+	editor.Undo_Redo.add_undo_method(editor.VoxelObjectRef, "update_mesh")
+	editor.Undo_Redo.commit_action()
--- addons/Voxel-Core/engine/VoxelObjectEditor/VoxelObjectEditorTool/VoxelObjectEditorTools/Pick.gd
+++ addons/Voxel-Core/engine/VoxelObjectEditor/VoxelObjectEditorTool/VoxelObjectEditorTools/Pick.gd
@@ -14,10 +14,8 @@ func _init():
 
 func work(editor) -> void:
 	var voxel = editor.VoxelObjectRef.get_rvoxel(editor.Cursors[Vector3.ZERO].Selections[0])
-	print(voxel)
 	match typeof(voxel):
 		TYPE_DICTIONARY:
 			editor.set_palette(editor.Palette.get_selected_id(), voxel.duplicate(true))
 		TYPE_INT, TYPE_STRING:
-			print(voxel)
 			editor.set_palette(editor.Palette.get_selected_id(), voxel)
