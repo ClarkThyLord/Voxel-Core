103d19d8ba2591b54119a004945fb72345cbff51
ClarkThyLord <ccpotential@gmail.com>
2020-12-09 22:56:41 -0800

    progress on VoxelSet rework
    Clark was here :V

--- addons/Voxel-Core/classes/VoxelSet.gd
+++ addons/Voxel-Core/classes/VoxelSet.gd
@@ -6,42 +6,45 @@ class_name VoxelSet, "res://addons/Voxel-Core/assets/classes/VoxelSet.png"
 
 
 # Declarations
-# Emitted when VoxelSet has had a voxel added/ set / removed
+# Emitted when VoxelSet has had a voxel added / set / removed
 signal updated_voxels
-# Emitted when uv or texture is modified
-signal updated_texture
+# Emitted when tiles data is changes
+signal updated_tiles
 
-# Names stored by their ID
-# TODO Rework naming system
-var Names := {}
+
 # Voxels stored by their ID
 var Voxels := {} setget set_voxels
-func get_ids() -> Array:
-	return Voxels.keys()
-
-func set_voxels(voxels : Dictionary, emit := true) -> void:
+# Sets voxels contained and calls on updated_voxels by default
+func set_voxels(voxels : Dictionary, update := true) -> void:
 	if Locked:
-		printerr("VoxelSet Locked")
+		printerr("VoxelSet is locked")
 		return
 	
 	Voxels = voxels
 	
-	if emit: updated_voxels()
+	if update: updated_voxels()
 
 
-# Flag indicating whether VoxelSet can be modifed
+# Flag indicating whether VoxelSet cannot be modified
 export(bool) var Locked := false
 
+# Calculated once per TileSize or Tiles change
 var UVScale := 0.0 setget set_uv_scale
+# Prevent external modificaciones to UV Scale
 func set_uv_scale(uv_scale : float) -> void: pass
 
+# Uniform size of tile
 export(float, 1, 1000000000, 1) var TileSize := 32.0 setget set_tile_size
+# Sets TileSize and calls on updated_textures by default
 func set_tile_size(tile_size : float, update := true) -> void:
 	TileSize = floor(clamp(tile_size, 1, 1000000000))
 	
 	if update: updated_textures()
 
+
+# Texture for VoxelSet
 export(Texture) var Tiles : Texture = null setget set_tiles
+# Sets Tiles and calls on updated_textures by default
 func set_tiles(tiles : Texture, update := true) -> void:
 	Tiles = tiles
 	
@@ -50,63 +53,56 @@ func set_tiles(tiles : Texture, update := true) -> void:
 
 
 # Core
+# Saves necessary data to meta
 func _save() -> void:
-	set_meta("Names", Names)
 	set_meta("Voxels", Voxels)
 
+# Loads necessary data from meta
 func _load() -> void:
-	if has_meta("Names"):
-		Names = get_meta("Names")
 	if has_meta("Voxels"):
 		Voxels = get_meta("Voxels")
 	updated_voxels()
 
 
-func _init(): call_deferred("_load")
+# Calls on _load as soon as feasible
+func _init():
+	call_deferred("_load")
 
 
 # Returns the next available ID
+# @return	int	:	Next available ID
 func get_next_id() -> int:
 	var ids := Voxels.keys()
 	ids.sort()
 	return (ids.back() + 1) if ids.size() > 0 else 0
 
-# Returns ID associated to given name
-func name_to_id(name : String) -> int:
-	return Names.get(name.to_lower(), -1)
-
-# Returns name associated to given ID
+# Returns name set to given ID
 func id_to_name(id : int) -> String:
-	for name in Names:
-		if Names[name] == id:
-			return name
-	return ""
+	return get_voxel(id).get("vsn", "")
 
 
-func name_voxel(id : int, name : String) -> void:
+
+# Sets given name to voxel with given ID, calls on updated_voxels by default
+# @param	id		:	int		:	
+# @param	name	:	String	:	
+# @param	update	:	bool	:	
+func name_voxel(id : int, name : String, update := true) -> void:
 	if Locked:
-		printerr("VoxelSet Locked")
+		printerr("VoxelSet is locked")
 		return
 	elif id < 0:
-		printerr("given id out of VoxelSet range")
+		printerr("given id is out of VoxelSet range")
 		return
 	elif name.empty():
-		printerr("given Voxel name invalid")
+		printerr("given voxel name is invalid")
 		return
 	
-	var prev := id_to_name(id)
-	if not prev.empty(): Names.erase(prev)
-	Names[name.to_lower()] = id
+	get_voxel(id)["vsn"] = name
+	
+	if update: updated_voxels()
 
-func unname_voxel(name : String) -> void:
-	if Locked:
-		printerr("VoxelSet Locked")
-		return
-	elif name.empty():
-		printerr("given Voxel name invalid")
-		return
-	
-	Names.erase(name)
+func unname_voxel(id : int) -> void:
+	pass
 
 
 func set_voxel(voxel : Dictionary, id := get_next_id(), name := "", update := true) -> int:
