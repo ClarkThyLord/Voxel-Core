2ee3658afd251f2bdad884bfaa8442c07266fcba
ClarkThyLord <ccpotential@gmail.com>
2020-12-13 18:35:40 -0800

    rework TilesViewer
    Clark was here :V

--- addons/Voxel-Core/controls/TilesViewer/TilesViewer.gd
+++ addons/Voxel-Core/controls/TilesViewer/TilesViewer.gd
@@ -4,40 +4,21 @@ extends TextureRect
 
 
 # Declarations
-signal selected(index)
-signal unselecting(index)
-signal unselected(index)
+signal selected(uv)
+signal unselected(uv)
 
 
-var hovered := -Vector2.ONE setget set_hovered
-func set_hovered(_hovered : Vector2, update := true) -> void:
-	hovered = _hovered
-	if update: self.update()
+var last_uv_hovered := -Vector2.ONE
+
 
 var Selections := [] setget set_selections
 func set_selections(selections : Array) -> void: pass
 
-export(bool) var SelectMode := false setget set_select_mode
-func set_select_mode(select_mode : bool) -> void:
-	SelectMode = select_mode
-	if not SelectMode: unselect_all()
-	update()
-
-export(int, 0, 1000000000) var SelectionMax := 0 setget set_selection_max
-func set_selection_max(selection_max : int) -> void:
-	selection_max = abs(selection_max)
-	if selection_max > 0 and selection_max < SelectionMax:
-		while Selections.size() > selection_max:
-			unselect(Selections.size() - 1)
-		update()
-	
-	SelectionMax = selection_max
-
-
-export(float, 1, 1000000000, 1) var TileSize := 32.0 setget set_texture_tile
-func set_texture_tile(tile_size : float, update := true) -> void:
-	TileSize = floor(clamp(tile_size, 1, 1000000000))
-	update()
+export(int, -1, 256) var AllowedSelections := 0 setget set_allowed_selections
+func set_allowed_selections(allowed_selections : int, update := true) -> void:
+	AllowedSelections = clamp(allowed_selections, -1, 256)
+	unselect_shrink()
+	if update: self.update()
 
 
 export(Color) var HoveredColor := Color(1, 1, 1, 0.6) setget set_hovered_color
@@ -56,92 +37,90 @@ func set_invalid_color(color : Color, update := true) -> void:
 	if update: self.update()
 
 
-export(Resource) var Voxel_Set = preload("res://addons/Voxel-Core/defaults/VoxelSet.tres") setget set_voxel_set
+export(Resource) var VoxelSetRef = null setget set_voxel_set
 func set_voxel_set(voxel_set : Resource, update := true) -> void:
-	if voxel_set is VoxelSet:
-		if Voxel_Set.is_connected("updated_texture", self, "correct"):
-			Voxel_Set.disconnect("updated_texture", self, "correct")
-		Voxel_Set = voxel_set
-		correct()
-		Voxel_Set.connect("updated_texture", self, "correct")
-		
-		if update: self.update()
-	elif typeof(voxel_set) == TYPE_NIL:
-		set_voxel_set(preload("res://addons/Voxel-Core/defaults/VoxelSet.tres"), update)
+	if not typeof(voxel_set) == TYPE_NIL and not voxel_set is VoxelSet:
+		printerr("VoxelViewer : Invalid Resource given expected VoxelSet")
+		return
+	
+	if is_instance_valid(VoxelSetRef):
+		if VoxelSetRef.is_connected("requested_refresh", self, "update"):
+			VoxelSetRef.disconnect("requested_refresh", self, "update") 
+	
+	VoxelSetRef = voxel_set
+	if is_instance_valid(VoxelSetRef):
+		VoxelSetRef.connect("requested_refresh", self, "update")
+		if is_instance_valid(VoxelSetRef.Tiles):
+			texture = VoxelSetRef.Tiles
+	else:
+		texture = null
+	
+	if update: self.update()
 
 
 
 # Helpers
 func world_to_uv(world : Vector2) -> Vector2:
-	return (world / TileSize).floor()
+	return (world / VoxelSetRef.TileSize).floor() if is_instance_valid(VoxelSetRef) and VoxelSetRef.UVReady else -Vector2.ONE
 
 func world_within_bounds(world : Vector2) -> bool:
-	if is_instance_valid(Voxel_Set) and Voxel_Set.Tiles:
-		var bounds = Voxel_Set.Tiles.get_size() / Voxel_Set.TileSize
+	if is_instance_valid(VoxelSetRef) and VoxelSetRef.UVReady:
+		var bounds = VoxelSetRef.Tiles.get_size() / VoxelSetRef.TileSize
 		return world.x >= 0 and world.y >= 0 and world.x < bounds.x and world.y < bounds.y
-	return true
+	return false
 
 
 
 # Core
-func correct() -> void:
-	if Voxel_Set:
-		texture = Voxel_Set.Tiles
-		TileSize = Voxel_Set.TileSize
-	update()
+func select(uv : Vector2, emit := true) -> void:
+	# TODO UV within bounds
+	if AllowedSelections != 0:
+		unselect_shrink(AllowedSelections - 1)
+		Selections.append(uv)
+		if emit:
+			emit_signal("selected", uv)
 
-
-func select(uv : Vector2) -> int:
-	if SelectMode:
-		if SelectionMax == 0 or Selections.size() < SelectionMax:
-			Selections.append(uv)
-		else:
-			unselect(Selections.size() - 1)
-			return select(uv)
-		emit_signal("selected", Selections.size() - 1)
-		return Selections.size() - 1
-	else: return -1
-
-func unselect(index : int) -> void:
-	emit_signal("unselecting", index)
-	if index > Selections.size():
-		printerr("unselect index out of range: ", index)
-		return
-	Selections.remove(index)
-	emit_signal("unselected", index)
+func unselect(uv : Vector2, emit := true) -> void:
+	if Selections.has(uv) and emit:
+		Selections.erase(uv)
+		emit_signal("unselected", uv)
 
 func unselect_all() -> void:
 	while not Selections.empty():
-		unselect(Selections.size() - 1)
+		unselect(Selections.back())
+
+func unselect_shrink(size := AllowedSelections, emit := true) -> void:
+	if size >= 0:
+		while Selections.size() > size:
+			unselect(Selections.back(), emit)
 
 
 func _gui_input(event : InputEvent):
 	if event is InputEventMouse:
-		var uv := world_to_uv(event.position)
-		if event is InputEventMouseMotion:
-			hovered = uv
-		elif SelectMode and event is InputEventMouseButton:
-			if world_within_bounds(uv) and event.button_index == BUTTON_LEFT and not event.is_pressed():
-				var index := Selections.find(uv)
-				if index == -1: select(uv)
-				else: unselect(index)
+		last_uv_hovered = world_to_uv(event.position)
+		if AllowedSelections != 0 and event is InputEventMouseButton:
+			if world_within_bounds(last_uv_hovered) and event.button_index == BUTTON_LEFT and not event.is_pressed():
+				if Selections.has(last_uv_hovered):
+					unselect(last_uv_hovered)
+				else:
+					select(last_uv_hovered)
 		update()
 
 
 func _draw():
-	if SelectMode:
-		for selection in Selections:
-			if not selection == -Vector2.ONE:
+	if is_instance_valid(VoxelSetRef) and VoxelSetRef.UVReady:
+		if AllowedSelections != 0:
+			for selection in Selections:
 				draw_rect(Rect2(
-				selection * TileSize,
-				Vector2(TileSize, TileSize)
+				selection * VoxelSetRef.TileSize,
+				VoxelSetRef.TileSize
 				), SelectionColor, false, 3)
-	
-	if hovered == -Vector2.ONE:
-		hint_tooltip = ""
-	else:
-		hint_tooltip = str(hovered)
-		draw_rect(Rect2(
-			hovered * TileSize,
-			Vector2(TileSize, TileSize)
-		), HoveredColor if world_within_bounds(hovered) else InvalidColor, false, 3)
+		
+		if last_uv_hovered == -Vector2.ONE:
+			hint_tooltip = ""
+		else:
+			hint_tooltip = str(last_uv_hovered)
+			draw_rect(Rect2(
+				last_uv_hovered * VoxelSetRef.TileSize,
+				VoxelSetRef.TileSize
+			), HoveredColor if world_within_bounds(last_uv_hovered) else InvalidColor, false, 3)
