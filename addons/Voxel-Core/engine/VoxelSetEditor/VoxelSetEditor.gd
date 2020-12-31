77797e5c9553ec0e5ca1877d3f42b3681fd86453
ClarkThyLord <ccpotential@gmail.com>
2020-12-31 14:54:54 -0800

    progress on refactor
    Clark was here :V

--- addons/Voxel-Core/engine/VoxelSetEditor/VoxelSetEditor.gd
+++ addons/Voxel-Core/engine/VoxelSetEditor/VoxelSetEditor.gd
@@ -3,66 +3,85 @@ extends ScrollContainer
 
 
 
-# Refrences
+## Signals
+# Emited when editor needs closing
+signal close
+
+
+
+## Exported Variables
+export(Resource) var voxel_set = null setget set_voxel_set
+
+
+
+## Public Variables
+var undo_redo : UndoRedo
+
+
+
+## Private Variables
+var import_file_path := ""
+
+
+
+## OnReady Variables
 onready var ImportFile := get_node("HBoxContainer/VBoxContainer/HBoxContainer/HBoxContainer/Import/ImportFile")
+
 onready var ImportHow := get_node("HBoxContainer/VBoxContainer/HBoxContainer/HBoxContainer/Import/ImportHow")
 
 onready var VoxelSetInfo := get_node("HBoxContainer/VBoxContainer/VoxelSetInfo")
 
 onready var VoxelInfo := get_node("HBoxContainer/VBoxContainer/VoxelInfo")
+
 onready var VoxelID := get_node("HBoxContainer/VBoxContainer/VoxelInfo/HBoxContainer/VoxelID")
+
 onready var VoxelName := get_node("HBoxContainer/VBoxContainer/VoxelInfo/HBoxContainer/VoxelName")
+
 onready var VoxelData := get_node("HBoxContainer/VBoxContainer/VoxelInfo/VoxelData")
 
 onready var VoxelSetViewer := get_node("HBoxContainer/VBoxContainer2/VoxelSetViewer")
 
 onready var VoxelInspector := get_node("HBoxContainer/VoxelInspector")
+
 onready var VoxelViewer := get_node("HBoxContainer/VoxelInspector/VoxelViewer")
 
 
 
-# Declarations
-signal close
+## Built-In Virtual Methods
+func _ready():
+	set_voxel_set(voxel_set)
+	
+	if not is_instance_valid(undo_redo):
+		undo_redo = UndoRedo.new()
+	VoxelSetViewer.undo_redo = undo_redo
+	VoxelViewer.undo_redo = undo_redo
 
 
-var Undo_Redo : UndoRedo
 
-
-export(Resource) var VoxelSetRef = null setget set_voxel_set
-func set_voxel_set(voxel_set : Resource, update := true) -> void:
+## Public Methods
+func set_voxel_set(value : Resource, update := true) -> void:
 	if not typeof(voxel_set) == TYPE_NIL and not voxel_set is VoxelSet:
 		printerr("VoxelSetEditor : Invalid Resource given expected VoxelSet")
 		return
 	
-	if is_instance_valid(VoxelSetRef):
-		if VoxelSetRef.is_connected("requested_refresh", self, "update_view"):
-			VoxelSetRef.disconnect("requested_refresh", self, "update_view") 
+	if is_instance_valid(voxel_set):
+		if voxel_set.is_connected("requested_refresh", self, "update_view"):
+			voxel_set.disconnect("requested_refresh", self, "update_view") 
 	
-	VoxelSetRef = voxel_set
-	if is_instance_valid(VoxelSetRef):
-		VoxelSetRef.connect("requested_refresh", self, "update_view")
+	voxel_set = value
+	if is_instance_valid(voxel_set):
+		voxel_set.connect("requested_refresh", self, "update_view")
 	if is_instance_valid(VoxelSetViewer):
-		VoxelSetViewer.VoxelSetRef = VoxelSetRef
+		VoxelSetViewer.voxel_set = voxel_set
 	
 	if update: update_view()
 
 
-
-# Core
-func _ready():
-	set_voxel_set(VoxelSetRef)
-	
-	if not is_instance_valid(Undo_Redo):
-		Undo_Redo = UndoRedo.new()
-	VoxelSetViewer.Undo_Redo = Undo_Redo
-	VoxelViewer.Undo_Redo = Undo_Redo
-
-
 func update_view() -> void:
-	if is_instance_valid(VoxelSetRef):
+	if is_instance_valid(voxel_set):
 		if is_instance_valid(VoxelSetInfo):
-			VoxelSetInfo.text = "Voxels:\t\t" + str(VoxelSetRef.Voxels.size())
-			VoxelSetInfo.text += "\nUV Ready:\t" + str(VoxelSetRef.UVReady)
+			VoxelSetInfo.text = "Voxels:\t\t" + str(voxel_set.Voxels.size())
+			VoxelSetInfo.text += "\nUV Ready:\t" + str(voxel_set.UVReady)
 		
 		if is_instance_valid(VoxelSetViewer):
 			var editing_single : bool = VoxelSetViewer.Selections.size() == 1
@@ -73,10 +92,10 @@ func update_view() -> void:
 				var id = VoxelSetViewer.Selections[0]
 				
 				VoxelID.text = str(id)
-				VoxelName.text = VoxelSetRef.id_to_name(id)
-				VoxelData.text = var2str(VoxelSetRef.get_voxel(id))
+				VoxelName.text = voxel_set.id_to_name(id)
+				VoxelData.text = var2str(voxel_set.get_voxel(id))
 				
-				VoxelViewer.setup(VoxelSetRef, id)
+				VoxelViewer.setup(voxel_set, id)
 	else:
 		if not is_instance_valid(VoxelSetInfo):
 			return
@@ -93,84 +112,93 @@ func _on_VoxelID_text_entered(new_id):
 	if new_id == VoxelSetViewer.Selections[0]: return
 	
 	var id = VoxelSetViewer.Selections[0]
-	var name = VoxelSetRef.id_to_name(id)
-	var voxel = VoxelSetRef.get_voxel(id)
-	Undo_Redo.create_action("VoxelSetEditor : Set voxel id")
-	Undo_Redo.add_do_method(VoxelSetRef, "erase_voxel", id)
-	Undo_Redo.add_undo_method(VoxelSetRef, "set_voxel", voxel, name, id)
+	var name = voxel_set.id_to_name(id)
+	var voxel = voxel_set.get_voxel(id)
+	undo_redo.create_action("VoxelSetEditor : Set voxel id")
+	undo_redo.add_do_method(voxel_set, "erase_voxel", id)
+	undo_redo.add_undo_method(voxel_set, "set_voxel", voxel, name, id)
 	
-	var _name = VoxelSetRef.id_to_name(new_id)
-	var _voxel = VoxelSetRef.get_voxel(new_id)
+	var _name = voxel_set.id_to_name(new_id)
+	var _voxel = voxel_set.get_voxel(new_id)
 	if not voxel.empty():
-		Undo_Redo.add_do_method(VoxelSetRef, "erase_voxel", new_id)
-	Undo_Redo.add_do_method(VoxelSetRef, "set_voxel", voxel, name, new_id)
+		undo_redo.add_do_method(voxel_set, "erase_voxel", new_id)
+	undo_redo.add_do_method(voxel_set, "set_voxel", voxel, name, new_id)
 	if voxel.empty():
-		Undo_Redo.add_undo_method(VoxelSetRef, "erase_voxel", new_id)
+		undo_redo.add_undo_method(voxel_set, "erase_voxel", new_id)
 	else:
-		Undo_Redo.add_undo_method(VoxelSetRef, "set_voxel", _voxel, _name, new_id)
+		undo_redo.add_undo_method(voxel_set, "set_voxel", _voxel, _name, new_id)
 	
-	Undo_Redo.add_do_method(VoxelSetRef, "request_refresh")
-	Undo_Redo.add_undo_method(VoxelSetRef, "request_refresh")
-	Undo_Redo.commit_action()
+	undo_redo.add_do_method(voxel_set, "request_refresh")
+	undo_redo.add_undo_method(voxel_set, "request_refresh")
+	undo_redo.commit_action()
 	VoxelSetViewer.unselect_all()
 	VoxelSetViewer.select(new_id)
+
 
 func _on_VoxelName_text_entered(new_name : String):
 	var voxel_id = VoxelSetViewer.Selections[0]
 	if new_name.empty():
-		var name = VoxelSetRef.id_to_name()
-		Undo_Redo.create_action("VoxelSetEditor : Remove voxel name")
-		Undo_Redo.add_do_method(VoxelSetRef, "unname_voxel", voxel_id)
-		Undo_Redo.add_undo_method(VoxelSetRef, "name_voxel", voxel_id, name)
+		var name = voxel_set.id_to_name()
+		undo_redo.create_action("VoxelSetEditor : Remove voxel name")
+		undo_redo.add_do_method(voxel_set, "unname_voxel", voxel_id)
+		undo_redo.add_undo_method(voxel_set, "name_voxel", voxel_id, name)
 	else:
-		Undo_Redo.create_action("VoxelSetEditor : Rename voxel")
-		Undo_Redo.add_do_method(VoxelSetRef, "name_voxel", voxel_id, new_name)
-		var id = VoxelSetRef.name_to_id(new_name)
+		undo_redo.create_action("VoxelSetEditor : Rename voxel")
+		undo_redo.add_do_method(voxel_set, "name_voxel", voxel_id, new_name)
+		var id = voxel_set.name_to_id(new_name)
 		if id > -1:
-			Undo_Redo.add_undo_method(VoxelSetRef, "name_voxel", id, new_name)
-		var name = VoxelSetRef.id_to_name(voxel_id)
+			undo_redo.add_undo_method(voxel_set, "name_voxel", id, new_name)
+		var name = voxel_set.id_to_name(voxel_id)
 		if not name.empty():
-			Undo_Redo.add_undo_method(VoxelSetRef, "name_voxel", voxel_id, name)
-	Undo_Redo.add_do_method(VoxelSetRef, "request_refresh")
-	Undo_Redo.add_undo_method(VoxelSetRef, "request_refresh")
-	Undo_Redo.commit_action()
+			undo_redo.add_undo_method(voxel_set, "name_voxel", voxel_id, name)
+	undo_redo.add_do_method(voxel_set, "request_refresh")
+	undo_redo.add_undo_method(voxel_set, "request_refresh")
+	undo_redo.commit_action()
 	VoxelSetViewer.unselect_all()
 	VoxelSetViewer.select(voxel_id)
 
 
-func _on_VoxelSetViewer_selected(voxel_id : int): update_view()
-func _on_VoxelSetViewer_unselected(voxel_id : int): update_view()
+func _on_VoxelSetViewer_selected(voxel_id : int):
+	update_view()
+
+
+func _on_VoxelSetViewer_unselected(voxel_id : int):
+	update_view()
 
 
 func open_ImportFile():
 	ImportFile.popup_centered()
 
+
 func open_ImportHow():
 	ImportHow.popup_centered()
+
 
 func close_ImportHow():
 	ImportHow.hide()
 
-var import_file_path := ""
+
 func _on_Import_file_selected(path):
 	import_file_path = path
 	open_ImportHow()
+
 
 func _on_Import_Append_pressed():
 	var result := Reader.read_file(import_file_path)
 	if result["error"] == OK:
 		for voxel in result["palette"]:
-			VoxelSetRef.set_voxel(result["palette"][voxel])
+			voxel_set.set_voxel(result["palette"][voxel])
 		
-		VoxelSetRef.request_refresh()
+		voxel_set.request_refresh()
 	else: printerr(result["error"])
 	close_ImportHow()
+
 
 func _on_Import_Replace_pressed():
 	var result := Reader.read_file(import_file_path)
 	if result["error"] == OK:
-		VoxelSetRef.Voxels = result["palette"]
+		voxel_set.Voxels = result["palette"]
 		
-		VoxelSetRef.request_refresh()
+		voxel_set.request_refresh()
 	else: printerr(result["error"])
 	close_ImportHow()
