d0506ee2adab90aba6a5870fbcd8f9a9189f35ed
ClarkThyLord <ccpotential@gmail.com>
2021-01-14 12:20:05 -0800

    layered noise
    Clark was here :V

--- examples/ProceduralWorld/ProceduralWorld.gd
+++ examples/ProceduralWorld/ProceduralWorld.gd
@@ -8,6 +8,14 @@ const voxel_set := preload("res://examples/ProceduralWorld/TiledVoxelSet.tres")
 
 
 ## Exported Variables
+export(int, 8, 64) var height_max := 16
+
+export(float, 0.01, 10.0, 0.01) var frequency := 1
+
+export(float, 0.01, 10.0, 0.01) var redistribution := 1.8
+
+export(int, 1, 36) var chunks := 8
+
 export(int, 16, 128) var chunk_size := 32
 
 export var marker_path : NodePath
@@ -22,7 +30,7 @@ var _chunks := {}
 
 
 ## OnReady Variables
-onready var chunks := get_node("Chunks")
+onready var Chunks := get_node("Chunks")
 
 
 
@@ -46,8 +54,18 @@ func _process(delta):
 				for x in range(chunk_size):
 					for z in range(chunk_size):
 						var grid_pos := _chunk_to_world(chunk_key) + Vector3(x, 0, z)
-						var noise := _noise.get_noise_3dv(grid_pos)
-						var height := range_lerp(noise, -1, 1, 0, 16)
+						var noise_1 := _noise.get_noise_3dv(grid_pos) * frequency
+						var noise_2 := 0.5 * _noise.get_noise_3dv(grid_pos * 2) * frequency
+						var noise_4 := 0.25 * _noise.get_noise_3dv(grid_pos * 4) * frequency
+						var noise := pow(
+								range_lerp(
+								noise_1 + noise_2 + noise_4,
+								-1, 1, 0, 1), 1.8) * redistribution
+						
+						var height := int(range_lerp(
+								noise,
+								0, 1, 0, height_max - 1)) + 1
+						
 						for y in range(height):
 							chunk.set_voxel(Vector3(x, y, z), 0)
 				chunk.update_mesh()
@@ -55,7 +73,7 @@ func _process(delta):
 				chunk.translation = chunk_key * chunk_size
 				chunk.translation -= Vector3(1, 0, 1) * (chunk_size / 2)
 				chunk.translation *= Voxel.VoxelWorldSize
-				chunks.add_child(chunk)
+				Chunks.add_child(chunk)
 				_chunks[chunk_key] = chunk
 
 
