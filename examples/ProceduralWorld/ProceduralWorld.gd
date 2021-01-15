31d8d547743dc1ac8457b5d06ad77ff2e0ea977d
ClarkThyLord <ccpotential@gmail.com>
2021-01-14 22:53:20 -0800

    added structures and more variance
    Clark was here :V

--- examples/ProceduralWorld/ProceduralWorld.gd
+++ examples/ProceduralWorld/ProceduralWorld.gd
@@ -16,6 +16,8 @@ export(float, 0.01, 10.0, 0.01) var amplitude := 1.0
 
 export(float, 0.01, 10.0, 0.01) var redistribution := 1.8
 
+export(float, 0.0, 1.0, 0.01) var structure_rate := 0.8
+
 export(int, 0, 10) var chunk_layers := 3
 
 export(int, 16, 128) var chunk_size := 16
@@ -30,6 +32,13 @@ var _noise : OpenSimplexNoise
 var _chunks := {}
 
 var _chunk_nodes := {}
+
+var _structures := {
+	"tree.oak":
+		preload("res://examples/ProceduralWorld/structures/tree.oak.tscn").instance(),
+	"tree.birch":
+		preload("res://examples/ProceduralWorld/structures/tree.birch.tscn").instance(),
+}
 
 
 
@@ -81,31 +90,53 @@ func _generate_chunk(chunk : Vector3) -> void:
 	chunk_node.uv_map = true
 	chunk_node.voxel_set = voxel_set
 	
+	var spawn_points := []
+	
 	for x in range(chunk_size):
 		for z in range(chunk_size):
-			var grid_pos := _chunk_to_world(chunk) + Vector3(x, 0, z)
-			var noise_1 := _noise.get_noise_3dv(grid_pos * frequency) * amplitude
-			var noise_2 := 0.5 * _noise.get_noise_3dv(grid_pos * 2 * frequency) * amplitude
-			var noise_4 := 0.25 * _noise.get_noise_3dv(grid_pos * 4 * frequency) * amplitude
+			var grid := _chunk_to_world(chunk) + Vector3(x, 0, z)
+			var noise_1 := _noise.get_noise_3dv(grid * frequency) * amplitude
+			var noise_2 := 0.5 * _noise.get_noise_3dv(grid * 2 * frequency) * amplitude
+			var noise_4 := 0.25 * _noise.get_noise_3dv(grid * 4 * frequency) * amplitude
 			var noise := pow(
-					range_lerp(
-					noise_1 + noise_2 + noise_4,
-					-1, 1, 0, 1), 1.8) * redistribution
+					range_lerp(noise_1 + noise_2 + noise_4, -1, 1, 0, 1),
+					redistribution)
 			
 			var altitude := int(range_lerp(
 					noise,
 					0, 1, 0, height))
 			altitude = altitude if altitude > 0 else 1
 			
+			grid.x = x
+			grid.z = z
 			for y in range(altitude):
+				grid.y = y
 				var voxel_id := -1
-				if y == altitude - 1:
-					voxel_id = 1
-				elif y < altitude - 3:
-					voxel_id = 3
-				elif y < altitude - 1:
-					voxel_id = 0
-				chunk_node.set_voxel(Vector3(x, y, z), voxel_id)
+				if y > 19:
+					if y == altitude - 1:
+						voxel_id = 7 if y > 24 else 4
+					else:
+						voxel_id = 3
+				else:
+					if y == altitude - 1:
+						voxel_id = 1
+					elif y < altitude - 3:
+						voxel_id = 3
+					elif y < altitude - 1:
+						voxel_id = 0
+				chunk_node.set_voxel(grid, voxel_id)
+				
+				if not (grid.y < 3 and grid.y == altitude - 1):
+					continue
+				var spawn_point := _noise.get_noise_3dv(Vector3(
+						floor(grid.x / 5), 0, floor(grid.z / 5)))
+				if spawn_point < structure_rate and not spawn_points.has(spawn_point) and randf() > structure_rate:
+					spawn_points.append(spawn_point)
+					var structure : VoxelMesh = _structures["tree." + ("oak" if randf() > 0.5 else "birch")]
+					for structure_grid in structure.get_voxels():
+						chunk_node.set_voxel(
+								grid + Vector3.UP + structure_grid,
+								structure.get_voxel_id(structure_grid))
 	chunk_node.update_mesh()
 	
 	chunk_node.translation = chunk * chunk_size
