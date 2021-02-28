06b6c0c715853aedf5c402387f92d83bc460273e
ClarkThyLord <ccpotential@gmail.com>
2021-02-27 21:32:33 -0800

    added VarReader
    Clark was here :V

---
+++ addons/voxel-core/classes/readers/var.gd
@@ -0,0 +1,26 @@
+class_name VarReader
+extends Reference
+# Var file reader
+
+
+
+# Public Methods
+# Reads var file, returns it's Variant content
+static func read(var_file : File) -> Dictionary:
+	var result := {
+		"error": OK,
+		"variant": var_file.get_var(),
+	}
+	
+	return result
+
+
+static func read_file(var_path : String) -> Dictionary:
+	var result := { "error": OK }
+	var var_file := File.new()
+	result["error"] = var_file.open(var_path, File.READ)
+	if result["error"] == OK:
+		result = read(var_file)
+	if var_file.is_open():
+		var_file.close()
+	return result
