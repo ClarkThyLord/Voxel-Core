8eb53043fd8da2bfc564adf4e7fdbb258c389488
ClarkThyLord <ccpotential@gmail.com>
2022-01-14 14:44:42 -0800

    TilePickerMenu

--- addons/voxel-core/controls/menus/tile_picker_menu/tile_picker_menu.gd
+++ addons/voxel-core/controls/menus/tile_picker_menu/tile_picker_menu.gd
@@ -1,16 +1,71 @@
-extends WindowDialog
+tool
+extends "res://addons/voxel-core/controls/menus/menu.gd"
+## Color Picker Menu Class
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+## Signals
+signal selected_tile(uv)
+
+signal unselected_tile(uv)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+## Exported Variables
+export(int, -1, 256) var selection_max := 0 setget set_selection_max
+
+export var hovered_color := Color(1, 1, 1, 0.6) setget set_hovered_color
+
+export var selected_color := Color.white setget set_selection_color
+
+export var invalid_color := Color.red setget set_invalid_color
+
+export(Resource) var voxel_set setget set_voxel_set
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+## OnReady Variables
+onready var tiles_viewer := get_node("VBoxContainer/ScrollContainer/TilesViewer")
+
+
+
+## Built-In Virtual Methods
+func _reay() -> void:
+	set_selection_max(selection_max)
+	set_hovered_color(hovered_color)
+	set_selection_color(selected_color)
+	set_invalid_color(invalid_color)
+	set_voxel_set(voxel_set)
+	
+	update_rect_min()
+
+
+## Public Methods
+func set_selection_max(value : int, update := true) -> void:
+	selection_max = value
+	if is_instance_valid(tiles_viewer):
+		tiles_viewer.set_selection_max(value, update)
+
+
+func set_hovered_color(value : Color, update := true) -> void:
+	hovered_color = value
+	if is_instance_valid(tiles_viewer):
+		tiles_viewer.set_hovered_color(value, update)
+
+
+func set_selection_color(value : Color, update := true) -> void:
+	selected_color = value
+	if is_instance_valid(tiles_viewer):
+		tiles_viewer.set_selection_color(value, update)
+
+
+func set_invalid_color(value : Color, update := true) -> void:
+	invalid_color = value
+	if is_instance_valid(tiles_viewer):
+		tiles_viewer.set_invalid_color(value, update)
+
+
+func set_voxel_set(value : Resource, update := true) -> void:
+	voxel_set = value
+	if is_instance_valid(tiles_viewer):
+		tiles_viewer.set_voxel_set(value, update)
