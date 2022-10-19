be433cf28f336bf45b896211f8231667e5d742cd
Christian Cuevas <ccpotential@gmail.com>
2022-10-18 20:32:59 -0700

    Revert breaking name changes

--- addons/voxel-core/classes/voxel.gd
+++ addons/voxel-core/classes/voxel.gd
@@ -6,7 +6,7 @@ extends Object
 #
 # NOTE: As this is a utility class, it is not designed to be instantiated. 
 # Instead, access all of its values and methods from anywhere like so:
-#	for face in Voxel.FACES:
+#	for face in Voxel.Faces:
 #		Voxel.get_face_color(..., face)
 #
 # Voxel-Core represents all voxels with dictionaries, however that doesn't mean
@@ -23,7 +23,7 @@ extends Object
 # Voxel Schema:
 # {
 #   name                 :   String           ~   Lowercase string, used as identifier by VoxelSet
-#   color                :   Color            ~   RGB color, default color used for all voxel faces
+#   color                :   Color            ~   RGB color, default color used for all voxel Faces
 #   colors               :   Dictionary = {   ~   Dictionary, contains RGB Color values paired with their respective Vector2 voxel face key
 #      Vector3.UP        :   Color
 #      Vector3.DOWN      :   Color
@@ -32,8 +32,8 @@ extends Object
 #      Vector3.FORWARD   :   Color
 #      Vector3.BACK      :   Color
 #   }
-#   tile                 :   Vector2          ~   Vector2, default tile used for all the voxel faces
-#   tiles                :   Dictionary = {   ~   tile position used on a per face bases, if not present fallback to voxel tile
+#   uv                 :   Vector2          ~   Vector2, default uv used for all the voxel Faces
+#   uvs                :   Dictionary = {   ~   uv position used on a per face bases, if not present fallback to voxel uv
 #      Vector3.UP        :   Vector2
 #      Vector3.DOWN      :   Vector2
 #      Vector3.RIGHT     :   Vector2
@@ -42,18 +42,18 @@ extends Object
 #      Vector3.BACK      :   Vector2
 #   }
 #   metallic             :   float            ~   Metallic value used for all voxel face's material, must be between 0.0 and 1.0 and if not present fallback is 0.0
-#   specular             :   float            ~   Specular value used for all voxel faces, must be between 0.0 and 1.0 and if not present fallback is 0.5
-#   roughness            :   float            ~   Roughness value used for all voxel faces, must be between 0.0 and 1.0 and if not present fallback is 1.0
-#   energy               :   float            ~   Emission energy value used for all voxel faces, must be between 0.0 and 16.0 and if not present fallback is 0.0
-#   energy_color         :   Color            ~   Emission color used for all voxel faces, if not present fallback is white
+#   specular             :   float            ~   Specular value used for all voxel Faces, must be between 0.0 and 1.0 and if not present fallback is 0.5
+#   roughness            :   float            ~   Roughness value used for all voxel Faces, must be between 0.0 and 1.0 and if not present fallback is 1.0
+#   energy               :   float            ~   Emission energy value used for all voxel Faces, must be between 0.0 and 16.0 and if not present fallback is 0.0
+#   energy_color         :   Color            ~   Emission color used for all voxel Faces, if not present fallback is white
 #   material             :   int              ~   ID of the VoxelSet material used for this voxel, if not present fallback is -1
 # }
 
 
 
 ## Constants
-# Lists of all voxel faces, and their adjacent directions
-const FACES := {
+# Lists of all voxel Faces, and their adjacent directions
+const Faces := {
 	Vector3.RIGHT: [ Vector3.FORWARD, Vector3.BACK, Vector3.DOWN, Vector3.UP ],
 	Vector3.UP: [ Vector3.LEFT, Vector3.RIGHT, Vector3.FORWARD, Vector3.BACK ],
 	Vector3.FORWARD: [ Vector3.LEFT, Vector3.RIGHT, Vector3.DOWN, Vector3.UP ],
@@ -116,63 +116,63 @@ static func remove_face_color(voxel : Dictionary, face : Vector3) -> void:
 			voxel.erase("colors")
 
 
-# Returns Dictionary representation of a tiled voxel
-# tile         :   Vector2                        :   tile to set
-# tiles        :   Dictionary<Vector3, Vector2>   :   face tile to set
+# Returns Dictionary representation of a uv voxel
+# uv         :   Vector2                        :   uv to set
+# uvs        :   Dictionary<Vector3, Vector2>   :   face uv to set
 # color      :   Color                          :   color to set
 # colors     :   Dictionary<Vector3, Color>     :   face colors to set
 # return     :   Dictionary<String, Variant>    :   Dictionary representing voxel
-static func tiled(tile : Vector2, tiles := {}, color := Color.white, colors := {}) -> Dictionary:
+static func uvd(uv : Vector2, uvs := {}, color := Color.white, colors := {}) -> Dictionary:
 	var voxel = colored(color, colors)
-	voxel["tile"] = tile
-	if not tiles.empty():
-		voxel["tiles"] = tiles
+	voxel["uv"] = uv
+	if not uvs.empty():
+		voxel["uvs"] = uvs
 	return voxel
 
 
-# Returns true if voxel has tile defined
-static func has_tile(voxel : Dictionary) -> bool:
-	return voxel.has("tile")
+# Returns true if voxel has uv defined
+static func has_uv(voxel : Dictionary) -> bool:
+	return voxel.has("uv")
 
 
-# Returns the defined tile within given voxel if present, otherwise returns a negative vector
-static func get_tile(voxel : Dictionary) -> Vector2:
-	return voxel.get("tile", -Vector2.ONE)
+# Returns the defined uv within given voxel if present, otherwise returns a negative vector
+static func get_uv(voxel : Dictionary) -> Vector2:
+	return voxel.get("uv", -Vector2.ONE)
 
 
-# Sets the given tile to the given voxel
-static func set_tile(voxel : Dictionary, tile : Vector2) -> void:
-	voxel["tile"] = tile
+# Sets the given uv to the given voxel
+static func set_uv(voxel : Dictionary, uv : Vector2) -> void:
+	voxel["uv"] = uv
 
 
-# Removes tile from given voxel
-static func remove_tile(voxel : Dictionary) -> void:
-	voxel.erase("tile")
+# Removes uv from given voxel
+static func remove_uv(voxel : Dictionary) -> void:
+	voxel.erase("uv")
 
 
-# Returns true if voxel has specified tile at given face
-static func has_face_tile(voxel : Dictionary, face : Vector3) -> bool:
-	return voxel.has("tiles") and voxel["tiles"].has(face)
+# Returns true if voxel has specified uv at given face
+static func has_face_uv(voxel : Dictionary, face : Vector3) -> bool:
+	return voxel.has("uvs") and voxel["uvs"].has(face)
 
 
-# Returns the defined tile at given face if present, otherwise returns tile
-static func get_face_tile(voxel : Dictionary, face : Vector3) -> Vector2:
-	return voxel["tiles"].get(face, get_tile(voxel)) if voxel.has("tiles") else get_tile(voxel)
+# Returns the defined uv at given face if present, otherwise returns uv
+static func get_face_uv(voxel : Dictionary, face : Vector3) -> Vector2:
+	return voxel["uvs"].get(face, get_uv(voxel)) if voxel.has("uvs") else get_uv(voxel)
 
 
-# Sets the given tile at the given face to the given voxel
-static func set_face_tile(voxel : Dictionary, face : Vector3, tile : Vector2) -> void:
-	if not voxel.has("tiles"):
-		voxel["tiles"] = {}
-	voxel["tiles"][face] = tile
+# Sets the given uv at the given face to the given voxel
+static func set_face_uv(voxel : Dictionary, face : Vector3, uv : Vector2) -> void:
+	if not voxel.has("uvs"):
+		voxel["uvs"] = {}
+	voxel["uvs"][face] = uv
 
 
-# Removes tile at given face from given voxel
-static func remove_face_tile(voxel : Dictionary, face : Vector3) -> void:
-	if voxel.has("tiles"):
-		voxel["tiles"].erase(face)
-		if voxel["tiles"].empty():
-			voxel.erase("tiles")
+# Removes uv at given face from given voxel
+static func remove_face_uv(voxel : Dictionary, face : Vector3) -> void:
+	if voxel.has("uvs"):
+		voxel["uvs"].erase(face)
+		if voxel["uvs"].empty():
+			voxel.erase("uvs")
 
 
 # Returns true if given name is valid
@@ -290,14 +290,14 @@ static func clean(voxel : Dictionary) -> void:
 	if not is_valid_name(get_name(voxel)):
 		remove_name(voxel)
 	
-	if get_tile(voxel) == get_tile({}):
-		remove_tile(voxel)
+	if get_uv(voxel) == get_uv({}):
+		remove_uv(voxel)
 	
-	for face in FACES:
+	for face in Faces:
 		if get_face_color(voxel, face) == get_color({}):
 			remove_face_color(voxel, face)
-		if get_face_tile(voxel, face) == get_tile({}):
-			remove_face_tile(voxel, face)
+		if get_face_uv(voxel, face) == get_uv({}):
+			remove_face_uv(voxel, face)
 	
 	if get_material(voxel) == get_material({}):
 		remove_material(voxel)
